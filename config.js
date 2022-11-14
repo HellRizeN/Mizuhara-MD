4a3745ebe2f3cf2b624380c1a3f76b9714e4c18d
ꜱᴀʏ.ꜱᴄᴏᴛᴄʜ <118150799+HellRizeN@users.noreply.github.com>
2022-11-14 11:15:37 +0530

    ꜱᴀʏ.ꜱᴄᴏᴛᴄʜ 💔

---
+++ config.js
@@ -0,0 +1,114 @@
+const fs = require("fs")
+const chalk = require("chalk")
+
+global.available = true
+global.autoReadGc = true
+global.autoReadAll = false
+global.antitags = true
+
+
+global.Owner = ['919172673114']
+global.OwnerNumber = ['919172673114']
+global.ownertag = ['919172673114']
+global.BotName = "Chizuru Mizuhara"
+global.packname = "Ox.Itachi < 3"
+global.author = "Say.Scotch"
+global.OwnerName = "Scotch"
+global.BotSourceCode = "Sorry I can't give"
+global.SupportGroupLink = "https://chat.whatsapp.com/JBDzYnTIrawLBptciuMiFA"
+global.sessionName = "session"
+
+// Prefix //
+// If you want to change the prefix, change it and also change all switch's button id from Core.js  otherwise buttons will not work //
+global.prefa = ['-']
+
+global.location = "Mumbai, India"
+global.reactmoji = "❤️"
+global.themeemoji = "💖"
+global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
+global.websitex = "sorry i can't give"
+global.lolhuman = "KaysaS"
+
+
+global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
+global.Thumb = fs.readFileSync("./Assets/pic9.jpg")
+global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg")
+global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg")
+
+
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+
+global.mess = {
+    jobdone: 'Job done...',
+    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+    botowner: 'Only my *Owner* can use this command, Baka!',
+    grouponly: 'This command is only made for *Groups*, Baka!',
+    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botonly: 'Only the *Bot itself* can use this command!',
+    waiting:'*Baka...*',
+    nolink: 'Please provide me *link*, Baka!',
+    error: 'An error occurd!',
+    banned: 'You are *Banned* fron using commands!',
+    bangc: 'This Group is *Banned* from using Commands!',
+    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    
+}
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 95,
+   goldawal: 30,
+   emeraldawal: 8,
+   umpanawal: 10,
+   potionawal: 5
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+
+
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+global.APIKeys = {
+	'https://zenzapis.xyz': '9bf11f69f259',
+}
+
+
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
