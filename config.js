1647aa38b57f9eef88a827d1ce5e63cbe5ef16d5
ꜱᴀʏ.ꜱᴄᴏᴛᴄʜ <118150799+HellRizeN@users.noreply.github.com>
2022-11-14 19:18:59 +0530

    Create config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ global.websitex = "sorry i can't give"
 global.lolhuman = "KaysaS"
 
 
-global.BotLogo = fs.readFileSync("./Assets/pic1.jpg")
+global.BotLogo = fs.readFileSync(".//pic1.jpg")
 global.Thumb = fs.readFileSync("./Assets/pic9.jpg")
 global.Thumb1 = fs.readFileSync("./Assets/pic5.jpg")
 global.ErrorPic = fs.readFileSync("./Assets/pic7.jpg")
